63b2ee6c26b3883af959f647bef6d4a82989c197
ApoorvSingh <apoorv.git@idesignmylife.net>
2026-06-05 03:01:05 +0000

    Initial student enrollment system

---
+++ enrollment_system/enrollment_system/doctype/enrollment/enrollment.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2026, Apoorv Singh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Enrollment", {
+	course_fee(frm) {
+		frm.set_value(
+			"total_fee",
+			(frm.doc.course_fee || 0) + (frm.doc.registration_fee || 0)
+		);
+	},
+
+	registration_fee(frm) {
+		frm.set_value(
+			"total_fee",
+			(frm.doc.course_fee || 0) + (frm.doc.registration_fee || 0)
+		);
+	}
+});
